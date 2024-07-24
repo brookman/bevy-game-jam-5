65682e83ca0135c8ab1416f5d91e8639e1ad260b
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2024-07-24 21:16:14 +0200

    feat: disable TAA only on WASM

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,5 @@
 use bevy::core_pipeline::bloom::BloomSettings;
-use bevy::core_pipeline::experimental::taa::TemporalAntiAliasPlugin;
+use bevy::core_pipeline::experimental::taa::{TemporalAntiAliasBundle, TemporalAntiAliasPlugin};
 use bevy::core_pipeline::tonemapping::Tonemapping;
 use bevy::pbr::{ScreenSpaceAmbientOcclusionBundle, ScreenSpaceReflectionsBundle};
 use bevy::prelude::*;
@@ -19,35 +19,35 @@ fn spawn(mut commands: Commands) {
     let focus = Vec3::new(0.0, 0.0, 0.0);
     let radius = Some((translation - focus).length());
 
-    commands
-        .spawn((
-            Camera3dBundle {
-                transform: Transform::from_translation(translation).looking_at(focus, Vec3::Y),
-                projection: Projection::Perspective(PerspectiveProjection {
-                    near: 0.001,
-                    far: 5.0,
-                    ..default()
-                }),
-                tonemapping: Tonemapping::BlenderFilmic,
-                camera: Camera {
-                    hdr: true,
-                    ..default()
-                },
+    let mut command = commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_translation(translation).looking_at(focus, Vec3::Y),
+            projection: Projection::Perspective(PerspectiveProjection {
+                near: 0.001,
+                far: 5.0,
+                ..default()
+            }),
+            tonemapping: Tonemapping::BlenderFilmic,
+            camera: Camera {
+                hdr: true,
                 ..default()
             },
-            BloomSettings::NATURAL,
-            PanOrbitCamera {
-                focus,
-                radius,
-                button_pan: MouseButton::Right,
-                button_orbit: MouseButton::Middle,
-                orbit_smoothness: 0.0,
-                pan_smoothness: 0.0,
-                zoom_smoothness: 0.0,
-                ..default()
-            },
-        ))
-        .insert(ScreenSpaceAmbientOcclusionBundle::default())
-        .insert(ScreenSpaceReflectionsBundle::default());
-    // .insert(TemporalAntiAliasBundle::default());
+            ..default()
+        },
+        BloomSettings::NATURAL,
+        PanOrbitCamera {
+            focus,
+            radius,
+            button_pan: MouseButton::Right,
+            button_orbit: MouseButton::Middle,
+            orbit_smoothness: 0.0,
+            pan_smoothness: 0.0,
+            zoom_smoothness: 0.0,
+            ..default()
+        },
+    ));
+    command.insert(ScreenSpaceAmbientOcclusionBundle::default());
+    command.insert(ScreenSpaceReflectionsBundle::default());
+    #[cfg(not(target_arch = "wasm32"))]
+    command.insert(TemporalAntiAliasBundle::default());
 }
